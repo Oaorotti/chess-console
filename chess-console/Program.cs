fb481428b3e3166000162dee31a13f9048a702fe
Joao Souza <joaowsouza1337@gmail.com>
2023-06-09 02:00:20 -0300

    initial commit

---
+++ chess-console/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace chess_console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
